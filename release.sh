7c3e8f74067317d8944ccf84f1b77447914a4192
Coppermine <229118171+Coppermine-factorio@users.noreply.github.com>
2017-12-14 22:53:04 +0000

    Update release script
    Taken from other mod

--- release.sh
+++ release.sh
@@ -2,5 +2,16 @@
 
 cd $(dirname $0)
 release_name=$(basename $PWD)
+full_release_path=$HOME/projects/factorio/factorio-mods/$release_name.zip
+
+if [ -e "$full_release_path" ]
+then
+  printf "Release file already exists\n" >&2
+  exit 1
+fi
+
 cd ..
-zip -r ~/tmp/factorio-mods/$release_name.zip $release_name/[^.rT]*
+
+swap_files=$(find $release_name -name '*.sw[po]')
+
+zip -r "$full_release_path" $release_name/[^.rT]* ${swap_files:+-x} $swap_files
